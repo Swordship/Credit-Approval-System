91107240ba5daf1cbd4f5ce1c62bd610e8d5bcec
Monish <monish635112@gmail.com>
2026-02-05 08:37:39 +0530

    change in the settings.py for postgres confiration to work on with and made and implemented migrations of both the customer and loans tables

--- credit_system/credit_system/settings.py
+++ credit_system/credit_system/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'credit_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'credit_system_db',      # Database name
+        'USER': 'postgres',      # Your PostgreSQL username
+        'PASSWORD': '12345678',  # Your PostgreSQL password
+        'HOST': 'localhost',      # Usually 'localhost'
+        'PORT': '5432',      # Usually '5432'
     }
 }
 
