31c6097c7d6c39f9fd291efa0d4e157d9f1ab9c4
Monish <monish635112@gmail.com>
2026-02-10 19:42:16 +0530

    feat : just add the path of the api endpoint in the urls

--- credit_system/loans/urls.py
+++ credit_system/loans/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     path('view-loan/<int:loan_id>/', views.view_loan, name='view_loan'),
     path('view-loans/<int:customer_id>/', views.view_loans, name='view_loans'),
     path('register/', views.register_customer, name='register'),
-    path ('check-eligibility/', views.check_eligibility, name='check_eligibility')
+    path ('check-eligibility/', views.check_eligibility, name='check_eligibility'),
+    path('create-loan/', views.create_loan, name='create_loan'),
 ]
